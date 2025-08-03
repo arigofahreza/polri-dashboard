4f21436e9bb9ceef34fdfe732540cd7c64fde43d
arigofahreza <arigoarg@gmail.com>
2025-08-03 22:29:30 +0700

    refactor: change cors

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.database import database
 from app.routes import auth, chart, report
 
 from app.database import metadata, engine
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all HTTP headers
+)
 
 # Include routes
 app.include_router(auth.router)
